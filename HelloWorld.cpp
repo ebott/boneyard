35f52ea738800067afc9ed20e266d69c21940169
eric.bott <eric.d.bott@gmail.com>
2013-06-10 15:08:53 -0700

    uploading for practice of practice

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "Hello Bitches!" << std::endl;
+   int x;
+   x = 15;
+
+   for ( int i = 0; i < 10; i++)
+   {
+      x++;
+      std::cout << x << std::endl;
+   }   
+   return 0;
+}
