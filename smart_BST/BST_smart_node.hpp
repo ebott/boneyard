10c5e75022170964926f29e6ccd1a9c315a8c0f8
eric.bott <eric.d.bott@gmail.com>
2013-09-04 14:24:21 -0700

    adding smart BST things

--- smart_BST/BST_smart_node.hpp
+++ smart_BST/BST_smart_node.hpp
@@ -13,22 +13,18 @@ struct BST_smart_node
 
    ~BST_smart_node()
    {
-      // should this be a private fcn????
-      // this method will delete node its called on and ALL
-      // ITS CHILDREN!  we want this, no??
-      //
-      // Then deleteing the larger tree struct is simply a
-      // metter of deleting the root;
-
-      //recursive outline:
-      //1)call this delete routine on your children (if they
-      //exist).
-      //2)delete yourself
+      delete r_child_;
+      delete l_child_;
    }
-
+ 
    int key() const
    {
       return key_;
+   }
+
+   void key(int new_key)
+   {
+      key_ = new_key;
    }
 
    string value() const
@@ -71,96 +67,116 @@ struct BST_smart_node
       this->parent_ = new_parent;
    }
 
+   template <typename FunType1, typename FunType2>
+   void binary_trans
+      (int target_key, FunType1 hit_fcn, FunType2 miss_fcn)
+   {
+      BST_smart_node* curr_node = this;
+      bool searching = true;
+      while(searching == true)
+      {
+         if(target_key == curr_node->key())
+         {
+            hit_fcn(curr_node);
+            searching = false;
+         }
+         else if(target_key < curr_node->key())
+         {
+            //move left or return
+            if(curr_node->l_child())
+            {
+               curr_node = curr_node->l_child();  
+            }
+            else 
+            {
+               miss_fcn(curr_node);
+               searching = false;
+            }
+         }
+         else if(target_key > curr_node->key())
+         {
+            //move right or return
+            if(curr_node->r_child())
+            {
+               curr_node = curr_node->r_child();  
+            }
+            else
+            {
+               miss_fcn(curr_node);
+               searching = false;
+            }
+         }
+         else
+         {
+            //we shoud never get here
+            throw "ack!";
+         }
+      }
+
+   }
+
+   template <typename FunType>
+   void DFT(FunType visit)
+   {
+      BST_smart_node* curr_node = this;
+      if(curr_node == nullptr)
+      {
+         return;
+      }
+
+      curr_node->l_child()->DFT(visit);
+      visit(curr_node);
+      curr_node->r_child()->DFT(visit);
+   }
+
+   template <typename FunType>
+   void BFT(FunType visit)
+   {
+      BST_smart_node* curr_node = this;
+      queue<BST_smart_node*>  node_queue;
+      node_queue.push(curr_node);
+
+      while(node_queue.size() != 0)
+      {
+         if(curr_node->l_child())
+         {
+            node_queue.push(curr_node->l_child());
+         }
+         if(curr_node->r_child())
+         {
+            node_queue.push(curr_node->r_child());
+         }
+
+         visit(curr_node);
+         node_queue.pop();
+         curr_node = node_queue.front();
+      }
+   }
+
+   BST_smart_node* in_order_successor()
+   {
+      //find the left-most child of right subtree
+      BST_smart_node* curr_node = this->r_child();
+      bool done = false;
+      while(done == false)
+      {
+         if(curr_node->l_child() != nullptr)
+         {
+            curr_node = curr_node->l_child();
+         }
+         else
+         {
+            done = true;
+         }
+      }
+      return curr_node;
+   }
 
    private:
       int key_;
       string value_;
-      struct BST_smart_node* parent_;
-      struct BST_smart_node* l_child_;
-      struct BST_smart_node* r_child_;
+      BST_smart_node* parent_;
+      BST_smart_node* l_child_;
+      BST_smart_node* r_child_;
 
-      template <typename FunType1, typename FunType2>
-      binary_trans(int target_key, FunType1 hit_fcn, Funtype2 miss_fcn)
-      {
-         BST_smart_node* curr_node = this;
-         bool searching = true;
-         while(searching == true)
-         {
-            if(target_key == curr_node->key())
-            {
-               hit_fcn(curr_node);
-               searching = false;
-            }
-            else if(target_key < curr_node->key())
-            {
-               //move left or return
-               if(curr_node->l_child())
-               {
-                  curr_node = curr_node->l_child();  
-               }
-               else 
-               {
-                  miss_fcn(curr_node);
-                  searching = false;
-               }
-            }
-            else if(target_key > curr_node->key())
-            {
-               //move right or return
-               if(curr_node->r_child())
-               {
-                  curr_node = curr_node->r_child();  
-               }
-               else
-               {
-                  miss_fcn(curr_node);
-                  searching = false;
-               }
-            }
-            else
-            {
-               //we shoud never get here
-               throw "ack!";
-            }
-         }
-
-      }
-
-      template <typename FunType>
-      void DFT(FunType visit) const
-      {
-         BST_smart_node* curr_node = this;
-         if(curr_node == nullptr)
-         {
-            return;
-         }
-
-         DFT(curr_node->l_child(), visit);
-         visit(curr_node);
-         DFT(curr_node->r_child(), visit);
-      }
-
-      template <typename FunType>
-      void BFT(FunType visit)
-      {
-         BST_smart_node* curr_node = this;
-         queue<BST_smart_node*>  node_queue;
-         node_queue.push(curr_node);
-
-         while(node_queue.size() != 0)
-         {
-            if(curr_node->l_child())
-            {
-               node_queue.push(curr_node->l_child());
-            }
-            if(curr_node->r_child())
-            {
-               node_queue.push(curr_node->r_child());
-            }
-
-            visit(curr_node);
-            node_queue.pop();
-            curr_node = node_queue.front();
-         }
-      }
 };
--- smart_BST/smart_BST.hpp
+++ smart_BST/smart_BST.hpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include "BST_smart_node.hpp"
-#include <queue>
-
+ 
 using namespace std;
 
 struct smart_BST
 {
    smart_BST(int root_key, string root_val)
-      :root_(unique_ptr<BST_node>(new BST_node(root_key,root_val)))
+      :root_(new BST_smart_node(root_key,root_val))
    {}
 
    ~smart_BST()
@@ -17,7 +16,7 @@ struct smart_BST
 
    void insert_node(int new_key, string new_val)
    {
-      auto insert_fcn = [&] (BST_node* n)
+      auto insert_fcn = [&] (BST_smart_node* n)
       {
          BST_smart_node* new_node = 
             new BST_smart_node(new_key,new_val);
@@ -36,8 +35,169 @@ struct smart_BST
       root_->binary_trans(new_key,insert_fcn,insert_fcn);
    }
 
+   void print_tree() const
+   {
+      auto print_node = [&] (BST_smart_node* n)
+      {
+         cout << n
+            << "  "
+            << n->key()
+            << "  "
+            << n->value()
+            << endl;
+      };
+
+      root_->BFT(print_node);
+   }
+
+   void inorder_print() const
+   {
+      cout << "Printing tree in-order..." << endl;
+      auto print_node = [&] (BST_smart_node* n)
+      {
+         cout << n
+            << "  "
+            << n->key()
+            << "  "
+            << n->value()
+            << endl;
+      };
+      root_->DFT(print_node);
+   }
+
+   void find_node(int key_wanted)
+   {
+      auto node_found = [&] (BST_smart_node* n)
+      { 
+         /*
+         cout << "Node found: "
+            << n
+            << "  "
+            << n->key()
+            << "  "
+            << n->value()
+            << endl;
+            */
+      };
+
+      auto node_not_found = [&] (BST_smart_node* n)
+      {
+         /*
+         cout << "Node not found, looking for: "
+            << key_wanted
+            << ".  Closest match: "
+            << n
+            << "  "
+            << n->key()
+            << "  "
+            << n->value()
+            << endl;
+            */
+      };
+
+      root_->binary_trans(key_wanted, node_found, node_not_found);
+   }
+
+   void delete_node (int doomed_key)
+   {
+      auto node_not_found = [&] (BST_smart_node* n)
+      {
+         cout << "No matching node to delete..." << endl;
+      };
+
+      auto node_delete = [&] (BST_smart_node* n)
+      {
+         cout << "Node found, deleting..." << endl;
+         
+         if((n->l_child() == nullptr) & (n->r_child() == nullptr))
+         {
+            leaf_delete(n);
+         }
+
+         if((n->l_child() == nullptr) ^ (n->r_child() == nullptr))
+         {
+            single_parent_delete(n);
+         }
+
+         if((n->l_child() != nullptr) & (n->r_child() != nullptr))
+         {
+            double_parent_delete(n);
+         }
+      };
+
+      root_->binary_trans(doomed_key, node_delete, node_not_found);
+   }
+
    private:
-      unique_ptr<BST_smart_node> root_;
+      BST_smart_node* root_;
+      
+      void leaf_delete(BST_smart_node* doomed_node)
+      {
+         cout << "Deleting leaf node..." << endl;
+         parent_pointer_shift(doomed_node, nullptr);
+         delete doomed_node;
+      }
 
+      void single_parent_delete(BST_smart_node* doomed_node)
+      {
+         cout << "Deleting a single parent node..." << endl;
+         if(doomed_node->r_child() != nullptr)
+         {
+            BST_smart_node* orphan = doomed_node->r_child();
+            parent_pointer_shift(doomed_node, orphan);
+         }
+         else if(doomed_node->l_child() != nullptr)
+         {
+            BST_smart_node* orphan = doomed_node->l_child();
+            parent_pointer_shift(doomed_node, orphan);
+         }
+         doomed_node->parent(nullptr);
+         doomed_node->r_child(nullptr);
+         doomed_node->l_child(nullptr);
+         delete doomed_node;
+      }
 
+      void double_parent_delete(BST_smart_node* doomed_node)
+      {
+         cout << "Swapping double parent node with successor"
+            << ", then deleting successor..." 
+            << endl;
+         
+         BST_smart_node* swap_node = doomed_node->in_order_successor();
+         //store info from swap_node into temp vars
+         //then delete swap_node
+         //then shift temp vars into n
+         int new_key = swap_node->key();
+         string new_value = swap_node->value();
+
+         if((swap_node->l_child() != nullptr)^(swap_node->r_child() != nullptr))
+         {
+            single_parent_delete(swap_node);
+         }
+         else
+         {
+            leaf_delete(swap_node);
+         }
+         doomed_node->key(new_key);
+         doomed_node->value(new_value);
+      }
+
+      void parent_pointer_shift(BST_smart_node* doomed_node,  
+               BST_smart_node* new_child)
+      {
+         BST_smart_node* parent_of_doomed = doomed_node->parent(); 
+         if(parent_of_doomed->r_child() == doomed_node)
+         {
+            parent_of_doomed->r_child(new_child);
+         } 
+         if(parent_of_doomed->l_child() == doomed_node)
+         {
+            parent_of_doomed->l_child(new_child);
+         }
+
+         if(new_child != nullptr)
+         {
+            new_child->parent(parent_of_doomed);
+         }
+      }
 };
